b0503e83e76fcb954b6c763e2fed25a7e88d17be
Jennifer <Rafter.jennifer@gmail.com>
2021-03-12 21:53:34 -0800

    created style for 3 screen sizes, learned some javascript code!

---
+++ app.js
@@ -0,0 +1,18 @@
+let x = 3;
+let y = 9;
+
+(x > y)
+
+if (x < y) {
+    console.log = 'they match';
+} else {
+    console.log = 'no luck';
+}
+
+const string = 'Puppies';
+
+
+for (const element of string) {
+    console.log(element);
+  }
+
